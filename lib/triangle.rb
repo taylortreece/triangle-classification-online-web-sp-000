59b7dbf8da214a440fda8f26ac2fef4f5d72c24d
Taylor Treece <taylor.treece@protonmail.com>
2021-01-25 16:51:19 -0600

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+
+  attr_accessor :side_1, :side_2, :side_3, :kind
+  
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
+    if (@side_1 + @side_2) <= @side_3 || (@side_2 + @side_3) <= @side_1 || (@side_1 + @side_3) <= @side_2
+      begin
+        raise TriangleError
+          puts error.message
+      end
+    elsif @side_1 == @side_2 && @side_1 == @side_3
+      self.kind = :equilateral
+    elsif @side_1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3
+      self.kind = :isosceles
+    elsif @side_1 != @side_2 || @side_2 != @side_3 || @side_3 != @side_1
+      self.kind = :scalene
+    end
+  end  
+
+  class TriangleError < StandardError
+    def message
+      "This is the error message."
+    end
+  end
+
+
 end
